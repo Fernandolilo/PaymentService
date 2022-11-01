a9b291b9acde1b057f302ee0f0809e13c90b0f25
Fernandolilo <fernando.sistenpro@outlook.com>
2022-11-01 07:22:17 -0300

    added objects in the contract - adicionado objetos no contrato

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,13 +1,32 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+import entities.Contract;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		System.out.println("Entre com os dados do contrato: ");
+		System.out.print("Número do contrato: ");
+
+		int number = sc.nextInt();
+
+		System.out.print("Data (dd/mm/yyyy): ");
+		LocalDate data = LocalDate.parse(sc.next(), fmt);
 		
+		System.out.print("Valor do contrato: ");
+		double totalValue =  sc.nextDouble();
+		
+		Contract obj = new Contract(number, data, totalValue);
+
 		sc.close();
 
 	}
